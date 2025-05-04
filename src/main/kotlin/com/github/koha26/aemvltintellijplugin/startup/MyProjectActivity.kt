47665d8db50f289760d6694afa33c0f9ad311e40
GitHub Action <action@github.com>
2025-05-04 18:58:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/koha26/aemvltintellijplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/koha26/aemvltintellijplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.koha26.aemvltintellijplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/test/kotlin/com/github/koha26/aemvltintellijplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/koha26/aemvltintellijplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.koha26.aemvltintellijplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.koha26.aemvltintellijplugin.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
